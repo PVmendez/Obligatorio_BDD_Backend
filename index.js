0c13085d63f0f4094490cb567178c24f1712eab6
PVmendez <valentino.mendez.rey@gmail.com>
2023-10-30 12:04:40 -0300

    first-commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3525;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.status(200).send({
+		message: 'GET Home route working fine!'
+	});
+});
+
+app.listen(port, function(){
+	console.log(`Server running in http://localhost:${port}`);
+	console.log('Defined routes:');
+	console.log('	[GET] http://localhost:3525/');
+});
