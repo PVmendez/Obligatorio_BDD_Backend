d395b262f9ef1353f93a4b4c46c5ee8a22cc92e4
PVmendez <valentino.mendez.rey@gmail.com>
2023-11-03 18:18:04 -0300

    added folder structure and getUsers

---
+++ app/routes/users.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import express from "express";
+import { getUsers } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/users", getUsers);
+
+export { router };
---
+++ app/utils/dataTest.js
@@ -0,0 +1,44 @@
+export const dataTest = {
+    "data":
+        [
+            {
+                "ci": 56058768,
+                "fullName": "Pablo Méndez",
+                "birthDate": "16/06/2002",
+                "healthCard": {
+                    "itHas": true,
+                    "dueDate": "01/09/2030",
+                    "receipt": ""
+                },
+                "residence": "Paraguay 1024",
+                "mail": "pablo.mendez@gmail.com",
+                "phone": "093415493",
+            },
+            {
+                "ci": 99999999,
+                "fullName": "Felipe Etchevers",
+                "birthDate": "02/01/2002",
+                "healthCard": {
+                    "itHas": true,
+                    "dueDate": "01/09/2030",
+                    "receipt": ""
+                },
+                "residence": "Santiago de Chile 2034",
+                "mail": "felipet@gmail.com",
+                "phone": "095342456",
+            },
+            {
+                "ci": 11111111,
+                "fullName": "Juan Ricetto",
+                "birthDate": "03/08/2002",
+                "healthCard": {
+                    "itHas": true,
+                    "dueDate": "01/09/2030",
+                    "receipt": ""
+                },
+                "residence": "18 de Julio 2341",
+                "mail": "juan2002@gmail.com",
+                "phone": "092123434",
+            }
+        ]
+}
