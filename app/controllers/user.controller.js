85b23350f82aaed191774d379b254177af0e625d
Juan Riccetto <juanmartinre@gmail.com>
2023-11-17 08:26:03 -0300

    Agregar usuarios

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -9,13 +9,64 @@ export const getUsers = (req, res) => {
   });
 };
 
+export const postUser = (req, res) => {
+  console.log(req.body);
+  const {
+    Ci,
+    Name,
+    Surname,
+    Birthdate,
+    Location,
+    Mail,
+    Phone,
+    IssueDate,
+    ExpirationDate,
+    Receipt,
+  } = req.body;
+
+  const formattedBirthdate = new Date(Birthdate).toISOString().slice(0, 10);
+  const formattedExpirationDate = new Date(ExpirationDate).toISOString().slice(0, 10);
+  const formatedIssueDate = '2023-11-14';
+  
+
+  // Insert into Funcionarios table
+  const query1 = `INSERT INTO Funcionarios (Ci, Nombre, Apellido, Fch_Nacimiento, Direccion, Email, Telefono) 
+                  VALUES ('${Ci}', '${Name}', '${Surname}', '${formattedBirthdate}', '${Location}', '${Mail}', '${Phone}')`;
+
+  const values1 = [Ci, Name, Surname, formattedBirthdate, Location, Mail, Phone];
+
+  // Insert into Carnet_Salud table
+  const query2 = `INSERT INTO Carnet_Salud (Ci, Fch_Emision, Fch_Vencimiento, Comprobante) 
+                  VALUES ('${Ci}', '${formatedIssueDate}', '${formattedExpirationDate}', '${Receipt}')`;
+
+    //const query2 = `SELECT * FROM Carnet_Salud;`
+
+  const values2 = [Ci, formatedIssueDate, formattedExpirationDate, Receipt];
+
+  // Execute the first query
+  conexion.query(query1, values1, (error1, results1, fields1) => {
+    if (error1) {
+      throw error1;
+    }
+
+    // Execute the second query
+    conexion.query(query2, values2, (error2, results2, fields2) => {
+      if (error2) {
+        throw error2;
+      }
+
+      res.status(200).json(results2);
+    });
+  });
+};
+
 export const createUsers = (req, res) => {
   const userCi = req.body.Ci;
   conexion.query(
     `UPDATE Funcionarios SET Actualizo = 0 WHERE Ci = ${userCi}`,
     (error, results, fields) => {
       if (error) throw error;
-      res.status(200).json({ message: 'Datos actualizados correctamente' });
-    } 
+      res.status(200).json({ message: "Datos actualizados correctamente" });
+    }
   );
 };
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,13 +1,15 @@
 "use strict";
 
 import express from "express";
-import { getUsers, createUsers } from "../controllers/user.controller.js";
+import { getUsers, createUsers, postUser } from "../controllers/user.controller.js";
 import { login } from "../controllers/login.controller.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", createUsers);
+
+router.post("/user", postUser);
 
 router.post("/login", login);
 
