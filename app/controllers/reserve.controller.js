162df7f39291212f715e54291f49ca1b52f4f9a6
PVmendez <valentino.mendez.rey@gmail.com>
2023-11-26 23:51:48 -0300

    feat: some things

--- app/controllers/reserve.controller.js
+++ app/controllers/reserve.controller.js
@@ -3,22 +3,37 @@
 import { conexion } from "../utils/dbConnection.js";
 
 export const getReserves = (req, res) => {
-  conexion.query("SELECT * FROM Reservas", (error, results, fields) => {
+  conexion.query("SELECT * FROM Agenda", (error, results, fields) => {
     if (error) throw error;
     res.status(200).json(results);
   });
 };
 
 export const createReserves = (req, res) => {
-    const { date, logId } = req.body;
-    const fechaOriginal = date;
-    const fechaConvertida = new Date(fechaOriginal).toISOString().split('T')[0];
-    conexion.query(
-      `INSERT INTO Reservas VALUES ('${fechaConvertida}', '${logId}')`,
-      (error, results, fields) => {
-        if (error) throw error;
-        res.status(200).json({ message: "Reserva creada exitosamente" });
-      } 
-    );
-  }; 
-  
+  const { date, ci } = req.body;
+  const fechaOriginal = date;
+  const fechaConvertida = new Date(fechaOriginal).toISOString().split("T")[0];
+
+  const orderNumber = 0;
+
+  conexion.query("SELECT * FROM Agenda", (error, results, fields) => {
+    if (error) throw error;
+    orderNumber = results[0];
+    console.log(orderNumber);
+  });
+
+  conexion.query(
+    `INSERT INTO Agenda VALUES ('${fechaConvertida}', '${ci}')`,
+    (error, results, fields) => {
+      if (error) throw error;
+      res.status(200).json({ message: "Reserva creada exitosamente" });
+    }
+  );
+};
+
+export const getPeriod = (req, res) => {
+    conexion.query("SELECT * FROM Periodos_Actualizacion", (error, results, fields) => {
+      if (error) throw error;
+      res.status(200).json(results);
+    });
+  };
--- app/routes/users.js
+++ app/routes/users.js
@@ -7,6 +7,7 @@ import {
   getUserTable,
   sendMail,
   postUser,
+  getEmployee,
 } from "../controllers/user.controller.js";
 import {
   createUsers,
@@ -16,6 +17,7 @@ import {
 import { verifyToken } from "../middleware/tokenVerify.js";
 import {
   createReserves,
+  getPeriod,
   getReserves,
 } from "../controllers/reserve.controller.js";
 
@@ -31,9 +33,11 @@ router.use((req, res, next) => {
 });
 
 router.get("/users", getUsers);
+router.get("/employee/:logId", getEmployee);
 router.get("/user-table", getUserTable);
 router.get("/login", getLogins);
 router.get("/reserves", getReserves);
+router.get("/period", getPeriod);
 
 router.post("/users", updateUsers);
 router.post("/employee", postUser);
